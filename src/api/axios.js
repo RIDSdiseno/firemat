37aebcf58584513e39c7270118504eb623202812
diseno1 <diseno@gmail.com>
2026-04-28 14:50:05 -0400

    Nuevo

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 // 🔥 INTERCEPTOR (LA CLAVE DE TODO)
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
-  if (token) {
+    if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-  }
+    }
 
-  return config;
+    return config;
 });
 
 export default instance;
