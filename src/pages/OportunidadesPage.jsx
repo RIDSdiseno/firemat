d4be6f090654e9660410e7aa9278897bb7ebc045
diseno1 <diseno@gmail.com>
2026-05-07 16:23:58 -0400

    OportunidadesPage

--- src/pages/OportunidadesPage.jsx
+++ src/pages/OportunidadesPage.jsx
@@ -270,7 +270,7 @@ function OportunidadesPage() {
     </form>
 )}
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
 
   {/* PROSPECTOS */}
   <div className="bg-neutral-100 rounded-xl p-4">
